dcc23e3e5dc4ecde42707bacf9fe7a00e3fdce29
Tartt <jeredwiley@gmail.com>
2024-04-25 21:53:00 -0500

    added pow()

--- spm-headers/include/msl/math.h
+++ spm-headers/include/msl/math.h
@@ -19,6 +19,8 @@ double fmod(double numer, double denom);
 double sqrt(double x);
 #define sqrtf(x) ((f32)sqrt(x))
 
+double pow(double base, double exponent);
+
 #define fabs(x) __fabs(x)
 #define fabsf(x) __fabsf(x)
 #define abs(x) __abs(x)
